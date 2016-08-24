b2c38a9361f03daa6b1f36cba302bb8961a2e4fb
Code Whisperer <daniel@danielstern.ca>
2016-08-24 18:37:20 -0400

    OK

--- demo/demo.js
+++ demo/demo.js
@@ -6,17 +6,21 @@ let ab = new house.ab();
 let port = process.env.port || 7777;
 
 ab.option()
-	.url(`/option1`);
+	.name(`Group A - elite controller offer page (multiple)`)
+	.url(`https://www.amazon.com/gp/offer-listing/B00ZDNNRB8/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00ZDNNRB8&linkCode=am2&tag=the-tech-reviewer-a-20&linkId=d3c4159691eac2c771d964aa7f295a29`)
+	.probability(1);
 
 ab.option()
-	.url(`/option2`);
+	.name(`Group B - elite controller main page`)
+	.url(`https://www.amazon.com/dp/B00ZDNNRB8/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00ZDNNRB8&linkCode=am2&tag=the-tech-reviewer-b-20&linkId=e57472e5aec289acc8bc472f9322e8eb`)
+	.probability(1);
 
 
-app.use('/test', ab.middleware());
+app.use('/elite-controller', ab.middleware());
 app.get('/',(req,res)=>{
 	res.send(
 		`
-			<a target=_blank href="test">Do the test</a>;
+			<a target=_blank href="elite-controller">Do the test</a>;
 		`
 	)
 })
