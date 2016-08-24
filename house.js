2d260175b6f0a2e9c0e4cd94904a8be1fb86638a
Code Whisperer <daniel@danielstern.ca>
2016-08-23 20:42:04 -0400

    Implemented test that will pass when this is middleware.

--- house.js
+++ house.js
@@ -1,19 +1,33 @@
+class Option {
+	constructor(name,url,probability = 1,...pings){
+		this.URLsToPingOn = [];
+	}
+	url(url){
+		this.directTo(url);
+	}
+	directTo(url){
+		this._destination = url;
+	}
+}
+
+class AB {
+	constructor(name,url,probability = 1,...pings){
+		this.options = [];
+	}
+	option(){
+		let option = new Option();
+		this.options.push(option);
+		return option;
+	}
+	name(name){
+		this._name = name;
+		return this;
+	}
+}
+
 class House {
 	constructor(){
-		this.ab = class {
-			constructor(name,url,probability = 1,...pings){
-				this.pings = [];
-			}
-			name(name){
-				this._name = name;
-			}
-			url(url){
-				this.directTo(url);
-			}
-			directTo(url){
-				this._destination = url;
-			}
-		};
+		this.ab = AB;
 	}
 }
 
--- test/house.js
+++ test/house.js
@@ -1,6 +1,8 @@
 let house = require('../house.js');
 let assert = require('chai').assert;
+let should = require('should');
 let expect = require('chai').expect;
+let express = require('express');
 
 describe("The House",()=>{
 	it("always wins",()=>{
@@ -8,6 +10,32 @@ describe("The House",()=>{
 	});
 
 	it("should have a property called AB",()=>{
-		expect(house.ab).to.be.defined;
+		expect(house.ab).not.to.be.undefined;
+		expect(new house.ab()).not.to.be.undefined;
+		//expect(house.ab).to.be.a.function();
+	});
+
+	it('should be express middleware',()=>{
+		try {
+			let ab = new house.ab();
+			let app = new express();
+			app.use('/ab',ab);
+		} catch (e) {
+			should.fail(e);
+		}
+
 	})
 });
+
+describe("The AB Test",()=>{
+	it("should return an option object when option is called");
+	it("should store any created options in an array");
+	it("should correctly route urls");
+});
+
+describe("The Option",()=>{
+	it("should change the name property");
+	it("should change the url property");
+	it("should change the direct to property");
+	it("should add another url to the ping property");
+})
